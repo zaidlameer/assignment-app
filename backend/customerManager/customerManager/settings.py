b237f7405890f6623b2b7d6a399bbea1f325632a
Zaid Lameer <119600757+zaidlameer@users.noreply.github.com>
2025-09-11 11:04:42 +0530

    Update settings.py

--- backend/customerManager/customerManager/settings.py
+++ backend/customerManager/customerManager/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     "default": dj_database_url.config(
         default=env("DATABASE_URL", default="postgres://postgres:postgres@localhost:5432/Assignment"),
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=not DEBUG,  # Only require SSL in production
     )
 }
 
