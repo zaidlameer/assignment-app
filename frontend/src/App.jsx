f1270fa8f262b4bbf8eefb2db8b7ac65585bb95e
Zaid Lameer <119600757+zaidlameer@users.noreply.github.com>
2025-09-15 13:02:11 +0530

    Revert "Update App.jsx"
    This reverts commit 057b9292c3b2a5849cbd1653b9a706ef08b81e96.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,9 +34,9 @@ function App() {
         <Route 
           path="/dashboard" 
           element={
-           
+            <ProtectedLayout>
               <Dashboard />
-            
+            </ProtectedLayout>
           } 
         />
         <Route 
